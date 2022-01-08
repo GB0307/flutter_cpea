f4bc1854cb461ff341aab155af7359fd4bf94cf4
Gabriel <gabrielborges031807@gmail.com>
2022-01-07 23:24:02 -0300

    implemented repository

--- lib/src/features/login/domain/usecases/get_cached_user_data.dart
+++ lib/src/features/login/domain/usecases/get_cached_user_data.dart
@@ -1,13 +1,16 @@
 import 'package:cpea/src/core/interfaces/response.dart';
 import 'package:cpea/src/core/interfaces/use_case.dart';
 import 'package:cpea/src/features/login/domain/entities/user_data.dart';
-import 'package:get/get.dart' hide Response;
+import 'package:cpea/src/features/login/domain/repositories/user_data_repository.dart';
 
 class GetCachedUserData extends IUseCase<UserData, String?> {
+  const GetCachedUserData(this._repo);
+
+  final IUserDataRepository _repo;
+
   @override
   // ignore: avoid_renaming_method_parameters
-  Response<UserData> call([String? tag]) {
-    // TODO: FETCH USER FROM REPO
-    return Response.from(Get.find<UserData>(tag: tag));
+  DResponse<UserData> call([String? tag]) {
+    return _repo.getCachedUserData();
   }
 }
