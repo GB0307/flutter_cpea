5a2ae0eb43579e96b55b32d61df4964553d6e35a
Gabriel <gabrielborges031807@gmail.com>
2022-01-08 09:35:14 -0300

    Refactor code into package

--- lib/dependency_injector.dart
+++ lib/dependency_injector.dart
@@ -1,5 +1,4 @@
 import 'package:cpea/firebase_options.dart';
-import 'package:cpea/src/core/interfaces/module.dart';
 import 'package:cpea/src/core/utils/env_variables.dart';
 import 'package:cpea/src/features/login/LoginModule.dart';
 import 'package:cpea/src/features/login/data/datasources/cached_user_data_datasource.dart';
@@ -9,6 +8,7 @@ import 'package:cpea/src/features/login/data/repositories/user_data_repository.d
 import 'package:cpea/src/features/login/data/repositories/user_repository.dart';
 import 'package:cpea/src/features/login/domain/repositories/user_repository.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:gbx_core/gbx_core.dart';
 
 Future injectDependencies() async {
   var firebaseInitializer = Firebase.initializeApp(
